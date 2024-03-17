db415d32c93040558659f048ea1422667ef5bd37
Magic-Alexander <aleksandr.sagalaev@mail.ru>
2024-03-18 00:03:54 +0300

    18.03.2024

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 
@@ -8,27 +9,29 @@ class AlienInvasion:
 
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
     
 
     def run_game(self):
 
         while True:
             for event in pygame.event.get():
-                print(event.type)
-                if event.type == 1027:
+                if event.type == pygame.QUIT:
                     sys.exit()
         
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             pygame.display.flip()
 
 if __name__ == '__main__':
 
     ai = AlienInvasion()
     ai.run_game()
-
 
 
 
