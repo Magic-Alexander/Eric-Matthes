aa6641141fd41d38c5d8881e87603a68d26597a6
Magic-Alexander <aleksandr.sagalaev@mail.ru>
2024-03-19 00:02:01 +0300

    19.03

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -19,14 +19,23 @@ class AlienInvasion: # create main class for Game
     def run_game(self):
 
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-        
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._check_events()
+            self._update_screen()
 
-            pygame.display.flip()
+    def _check_events(self):
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x +=1
+
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()                
+        pygame.display.flip()
 
 if __name__ == '__main__':
 
